e02e5ee3e25bedb682aa3fed3f3a742bd804505d
Matt Vertescher <mvertescher@gmail.com>
2019-01-03 00:45:45 -0500

    Add additional board configurations

--- build.rs
+++ build.rs
@@ -29,13 +29,37 @@ fn main() {
 /// Unfortunately, this must be kept in sync with the features defined in Cargo.toml.
 #[derive(Display, EnumIter, Debug)]
 enum Feature {
+    // F0
+    NUCLEO_F030R8,
+    NUCLEO_F042K6,
+    NUCLEO_F070RB,
+
+    // F4
     NUCLEO_F401RE,
     NUCLEO_F411RE,
     NUCLEO_F412RG,
+    NUCLEO_F412ZG,
+    NUCLEO_F429ZI,
+
+    // F7
+    NUCLEO_F722ZE,
+    NUCLEO_F746ZG,
+    NUCLEO_F756ZG,
+    NUCLEO_F767ZI,
+
+    // L4
     NUCLEO_L412KB,
+    NUCLEO_L412RB_P,
     NUCLEO_L432KC,
+    NUCLEO_L433RC_P,
     NUCLEO_L452RE,
+    NUCLEO_L452RE_P,
     NUCLEO_L476RG,
+    NUCLEO_L496ZG,
+    NUCLEO_L496ZG_P,
+    NUCLEO_L4A6ZG,
+    NUCLEO_L4R5ZI,
+    NUCLEO_L4R5ZI_P,
 }
 
 /// Determine which feature flag was enabled based on env variables set by Cargo
@@ -55,59 +79,54 @@ fn export_cfg(string: &str) {
     println!("cargo:rustc-cfg={}", string);
 }
 
+macro_rules! board {
+    ($form:ident, $family:ident, $flash_size:tt, $ram_size:tt) => {
+        {
+            let map = MemoryMap {
+                flash: (0x08000000, $flash_size.to_string()),
+                ram: (0x20000000, $ram_size.to_string()),
+            };
+            (Form::$form, Family::$family, map)
+        }
+    };
+}
+
 impl Feature {
-    /// Return information about the device feature
+    /// Return information about a specific board (feature)
     fn expand(&self) -> (Form, Family, MemoryMap) {
         match self {
-            Feature::NUCLEO_F401RE => {
-                let map = MemoryMap {
-                    flash: (0x08000000, "64K".to_string()),
-                    ram: (0x20000000, "256K".to_string()),
-                };
-                (Form::Nucleo64, Family::Stm32F4xx, map)
-            }
-            Feature::NUCLEO_F411RE => {
-                let map = MemoryMap {
-                    flash: (0x08000000, "512K".to_string()),
-                    ram: (0x20000000, "128K".to_string()),
-                };
-                (Form::Nucleo64, Family::Stm32F4xx, map)
-            }
-            Feature::NUCLEO_F412RG => {
-                let map = MemoryMap {
-                    flash: (0x08000000, "1M".to_string()),
-                    ram: (0x20000000, "256K".to_string()),
-                };
-                (Form::Nucleo64, Family::Stm32F4xx, map)
-            }
-            Feature::NUCLEO_L412KB => {
-                let map = MemoryMap {
-                    flash: (0x08000000, "128K".to_string()),
-                    ram: (0x20000000, "40K".to_string()),
-                };
-                (Form::Nucleo32, Family::Stm32L4xx, map)
-            }
-            Feature::NUCLEO_L432KC => {
-                let map = MemoryMap {
-                    flash: (0x08000000, "256K".to_string()),
-                    ram: (0x20000000, "64K".to_string()),
-                };
-                (Form::Nucleo32, Family::Stm32L4xx, map)
-            }
-            Feature::NUCLEO_L452RE => {
-                let map = MemoryMap {
-                    flash: (0x08000000, "512K".to_string()),
-                    ram: (0x20000000, "160K".to_string()),
-                };
-                (Form::Nucleo64, Family::Stm32L4xx, map)
-            }
-            Feature::NUCLEO_L476RG => {
-                let map = MemoryMap {
-                    flash: (0x08000000, "1M".to_string()),
-                    ram: (0x20000000, "128K".to_string()),
-                };
-                (Form::Nucleo64, Family::Stm32L4xx, map)
-            }
+            // F0
+            Feature::NUCLEO_F030R8 => board!(Nucleo64,  Stm32F0xx, "256K", "32K"),
+            Feature::NUCLEO_F042K6 => board!(Nucleo32,  Stm32F0xx, "32K",  "6K"),
+            Feature::NUCLEO_F070RB => board!(Nucleo64,  Stm32F0xx, "128K", "16K"),
+
+            // F4
+            Feature::NUCLEO_F401RE => board!(Nucleo64,  Stm32F4xx, "64K",  "256K"),
+            Feature::NUCLEO_F411RE => board!(Nucleo64,  Stm32F4xx, "512K", "128K"),
+            Feature::NUCLEO_F412RG => board!(Nucleo64,  Stm32F4xx, "1M",   "256K"),
+            Feature::NUCLEO_F412ZG => board!(Nucleo144, Stm32F4xx, "1M",   "256K"),
+            Feature::NUCLEO_F429ZI => board!(Nucleo144, Stm32F4xx, "2M",   "256K"),
+
+            // F7
+            Feature::NUCLEO_F722ZE => board!(Nucleo144, Stm32F7xx, "512K", "256K"),
+            Feature::NUCLEO_F746ZG => board!(Nucleo144, Stm32F7xx, "1M",   "320K"),
+            Feature::NUCLEO_F756ZG => board!(Nucleo144, Stm32F7xx, "1M",   "320K"),
+            Feature::NUCLEO_F767ZI => board!(Nucleo144, Stm32F7xx, "2M",   "512K"),
+
+            // L4
+            Feature::NUCLEO_L412KB   => board!(Nucleo32,  Stm32L4xx, "128K", "40K"),
+            Feature::NUCLEO_L412RB_P => board!(Nucleo64,  Stm32L4xx, "128K", "40K"),
+            Feature::NUCLEO_L432KC   => board!(Nucleo32,  Stm32L4xx, "256K", "64K"),
+            Feature::NUCLEO_L433RC_P => board!(Nucleo64,  Stm32L4xx, "256K", "64K"),
+            Feature::NUCLEO_L452RE   => board!(Nucleo64,  Stm32L4xx, "512K", "160K"),
+            Feature::NUCLEO_L452RE_P => board!(Nucleo64,  Stm32L4xx, "512K", "160K"),
+            Feature::NUCLEO_L476RG   => board!(Nucleo64,  Stm32L4xx, "1M",   "128K"),
+            Feature::NUCLEO_L496ZG   => board!(Nucleo64,  Stm32L4xx, "1M",   "320K"),
+            Feature::NUCLEO_L496ZG_P => board!(Nucleo144, Stm32L4xx, "1M",   "320K"),
+            Feature::NUCLEO_L4A6ZG   => board!(Nucleo144, Stm32L4xx, "1M",   "320K"),
+            Feature::NUCLEO_L4R5ZI   => board!(Nucleo144, Stm32L4xx, "2M",   "640K"),
+            Feature::NUCLEO_L4R5ZI_P => board!(Nucleo144, Stm32L4xx, "2M",   "640K"),
+
         }
     }
 }
@@ -117,7 +136,7 @@ impl Feature {
 enum Form {
     Nucleo32,
     Nucleo64,
-    Nucleo128,
+    Nucleo144,
 }
 
 impl fmt::Display for Form {
